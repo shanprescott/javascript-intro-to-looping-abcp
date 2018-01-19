0c457cd5592e253e5ede65dd58e5032a88e9bbcc
Shannon Prescott <shan.prescott@outlook.com>
2018-01-19 19:21:18 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,36 @@
+function forLoop(array){
+  for (var i = 0; i < 25; i++){
+    if (i === 1){
+      array.push(`I am 1 strange loop`);
+    }
+    else{
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  
+  return array;
+}
+
+function whileLoop(n){
+  var count = n;
+  
+  while(count > 0){
+    console.log(count);
+    count--;
+  }
+  
+  return 'done';
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  } while (maybeTrue() || array.length > 0)
+  
+  return array;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+  
